daa023f20cb15eb2ef2afadafa25f2bb36bd5fcb
Daniel Hanson <daniel.hanson@SendOutCards.com>
2016-09-20 11:35:11 -0600

    ensure qb com connection closed if soft time limit is exceeded

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ from quickbooks import QuickBooks, QuickBooksError
 logger = get_task_logger(__name__)
 
 
-@celery_app.task(name='qb_desktop.tasks.qb_requests', track_started=True, max_retries=5)
+@celery_app.task(name='qb_desktop.tasks.qb_requests', track_started=True, max_retries=5, soft_time_limit=3600)
 def qb_requests(request_list=None, initial=False, with_sides=True, app='quickbooks', days=3):
     """
     Always send a list of requests so we aren't opening and closing file more than necessary
@@ -31,78 +31,84 @@ def qb_requests(request_list=None, initial=False, with_sides=True, app='quickboo
             ], with_sides=False)
 
     """
-    qb = QuickBooks(**QB_LOOKUP)
-    qb.begin_session()
+    try:
+        qb = QuickBooks(**QB_LOOKUP)
+        qb.begin_session()
 
-    # process request list if it exists or just get open purchase orders
-    if request_list:
-        for entry in request_list:
-            try:
-                surrogate_key, model_name, request_body = entry
-                request_type, request_dict = request_body
-                response = qb.call(request_type, request_dictionary=request_dict)
-                if surrogate_key and request_dict:
-                    celery_app.send_task(
-                        'quickbooks.tasks.process_response',
-                        queue='quickbooks',
-                        args=[surrogate_key, model_name, response, app], expires=1800
-                    )
-            except Exception as e:
-                logger.error(e)
+        # process request list if it exists or just get open purchase orders
+        if request_list:
+            for entry in request_list:
+                try:
+                    surrogate_key, model_name, request_body = entry
+                    request_type, request_dict = request_body
+                    response = qb.call(request_type, request_dictionary=request_dict)
+                    if surrogate_key and request_dict:
+                        celery_app.send_task(
+                            'quickbooks.tasks.process_response',
+                            queue='quickbooks',
+                            args=[surrogate_key, model_name, response, app], expires=1800
+                        )
+                except Exception as e:
+                    logger.error(e)
 
-    if with_sides:
-        # process all appropriate purchase orders with soc_accounting and
-        # post all unposted purchase orders to snapfulfil once these are finished processing
-        for purchase_order in qb.get_purchase_orders(days=days):
-            celery_app.send_task(
-                'quickbooks.tasks.process_purchase_order',
-                queue='quickbooks',
-                args=[purchase_order], expires=1800
-            )
-    celery_app.send_task(
-        'quickbooks.tasks.post_purchase_orders_to_snapfulfil',
-        queue='quickbooks',
-        expires=1800
-    )
-    celery_app.send_task(
-            'quickbooks.tasks.process_preferences',
+        if with_sides:
+            # process all appropriate purchase orders with soc_accounting and
+            # post all unposted purchase orders to snapfulfil once these are finished processing
+            for purchase_order in qb.get_purchase_orders(days=days):
+                celery_app.send_task(
+                    'quickbooks.tasks.process_purchase_order',
+                    queue='quickbooks',
+                    args=[purchase_order], expires=1800
+                )
+        celery_app.send_task(
+            'quickbooks.tasks.post_purchase_orders_to_snapfulfil',
             queue='quickbooks',
-            args=[qb.get_preferences()], expires=1800
-    )
-    # making sure to end session and close file
-    del(qb)
+            expires=1800
+        )
+        celery_app.send_task(
+                'quickbooks.tasks.process_preferences',
+                queue='quickbooks',
+                args=[qb.get_preferences()], expires=1800
+        )
+        # making sure to end session and close file
+    finally:
+        del(qb)
 
 
-@celery_app.task(name='qb_desktop.tasks.get_items', track_started=True, max_retries=5)
+@celery_app.task(name='qb_desktop.tasks.get_items', track_started=True, max_retries=5, soft_time_limit=3600)
 def get_items(initial=False, days=3):
     """
     this task takes no arguments and just grabs every item in Quickbooks and sends a task to process the response for each item.  I will likely be adding argument for item type in the future.
     """
-    qb = QuickBooks(**QB_LOOKUP)
-    qb.begin_session()
-    for item in qb.get_items(initial=initial, days=days):
-        celery_app.send_task(
-            'quickbooks.tasks.process_item',
-            queue='quickbooks',
-            args=[item], expires=3600
-        )
-    del(qb)
+    try:
+        qb = QuickBooks(**QB_LOOKUP)
+        qb.begin_session()
+        for item in qb.get_items(initial=initial, days=days):
+            celery_app.send_task(
+                'quickbooks.tasks.process_item',
+                queue='quickbooks',
+                args=[item], expires=3600
+            )
+    finally:
+        del(qb)
 
 
-@celery_app.task(name='qb_desktop.tasks.get_checks', track_started=True, max_retries=5)
+@celery_app.task(name='qb_desktop.tasks.get_checks', track_started=True, max_retries=5, soft_time_limit=3600)
 def get_checks(initial=False, days=5, accounts=['uncleared', 'cleared']):
     """
     grab all cleared and uncleared Distributor checks
     """
-    qb = QuickBooks(**QB_LOOKUP)
-    qb.begin_session()
-    for check in qb.get_checks(initial=initial, days=days, accounts=accounts):
-        celery_app.send_task(
-            'quickbooks.tasks.process_check',
-            queue='quickbooks',
-            args=[check], expires=3600
-        )
-    del(qb)
+    try:
+        qb = QuickBooks(**QB_LOOKUP)
+        qb.begin_session()
+        for check in qb.get_checks(initial=initial, days=days, accounts=accounts):
+            celery_app.send_task(
+                'quickbooks.tasks.process_check',
+                queue='quickbooks',
+                args=[check], expires=3600
+            )
+    finally:
+        del(qb)
 
 
 @celery_app.task(name='qb_desktop.tasks.pretty_print', track_started=True, max_retries=5)
